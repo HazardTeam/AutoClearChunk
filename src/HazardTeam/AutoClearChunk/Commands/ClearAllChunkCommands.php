272ba7d776e7a23fedf98019ab343fa334780c07
AIPTU <anangp808@gmail.com>
2022-03-09 19:12:47 +0700

    Bump version UpdateNotifier

--- src/HazardTeam/AutoClearChunk/Commands/ClearAllChunkCommands.php
+++ src/HazardTeam/AutoClearChunk/Commands/ClearAllChunkCommands.php
@@ -1,39 +1,42 @@
 <?php
 
+declare(strict_types=1);
+
 namespace HazardTeam\AutoClearChunk\Commands;
 
 use HazardTeam\AutoClearChunk\AutoClearChunk;
 use pocketmine\command\Command;
-use pocketmine\player\Player;
 use pocketmine\command\CommandSender;
 use pocketmine\plugin\PluginOwned;
-use pocketmine\utils\TextFormat;
 
-class ClearAllChunkCommands extends Command implements PluginOwned {
+final class ClearAllChunkCommands extends Command implements PluginOwned
+{
+	private AutoClearChunk $plugin;
 
-	/** @var AutoClearChunk $plugin */
-    private $plugin;
-
-    /**
-     * ClearAllChunkCommands constructor.
-     * @param AutoClearChunk $plugin
-     */
-    public function __construct(AutoClearChunk $plugin) {
+	/**
+	 * ClearAllChunkCommands constructor.
+	 */
+	public function __construct(AutoClearChunk $plugin)
+	{
 		$this->plugin = $plugin;
-		parent::__construct("clearallchunk", "Clear Chunk Commands");
-        $this->setPermission("autoclearchunk.clearchunkall");
-    }
+		parent::__construct('clearallchunk', 'Clear Chunk Commands');
+		$this->setPermission('autoclearchunk.clearchunkall');
+	}
 
-    public function execute(CommandSender $sender, string $commandLabel, array $args): bool{
-    	if (!$this->testPermission($sender)) return false;
-    
+	public function execute(CommandSender $sender, string $commandLabel, array $args): bool
+	{
+		if (!$this->testPermission($sender)) {
+			return false;
+		}
+
 		// now clear the world chunk
 		// message clear chunk only goes to CommandSender
 		$this->getOwningPlugin()->clearAllChunk($sender);
-        return true;
+		return true;
 	}
-	
-	public function getOwningPlugin(): AutoClearChunk{
-        return $this->plugin;
-    }
+
+	public function getOwningPlugin(): AutoClearChunk
+	{
+		return $this->plugin;
+	}
 }
--- src/HazardTeam/AutoClearChunk/Commands/ClearChunkCommands.php
+++ src/HazardTeam/AutoClearChunk/Commands/ClearChunkCommands.php
@@ -1,46 +1,51 @@
 <?php
 
+declare(strict_types=1);
+
 namespace HazardTeam\AutoClearChunk\Commands;
 
 use HazardTeam\AutoClearChunk\AutoClearChunk;
 use pocketmine\command\Command;
-use pocketmine\player\Player;
 use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
 use pocketmine\plugin\PluginOwned;
 use pocketmine\utils\TextFormat;
+use function implode;
 
-class ClearChunkCommands extends Command implements PluginOwned {
+final class ClearChunkCommands extends Command implements PluginOwned
+{
+	private AutoClearChunk $plugin;
 
-	/** @var AutoClearChunk $plugin */
-    private $plugin;
-
-    /**
-     * ClearChunkCommands constructor.
-     * @param AutoClearChunk $plugin
-     */
-    public function __construct(AutoClearChunk $plugin) {
+	/**
+	 * ClearChunkCommands constructor.
+	 */
+	public function __construct(AutoClearChunk $plugin)
+	{
 		$this->plugin = $plugin;
-		parent::__construct("clearchunk", "Clear Chunk Commands");
-        $this->setPermission("autoclearchunk.clearchunk");
-    }
+		parent::__construct('clearchunk', 'Clear Chunk Commands');
+		$this->setPermission('autoclearchunk.clearchunk');
+	}
 
-    public function execute(CommandSender $sender, string $commandLabel, array $args): bool{
-    	if (!$this->testPermission($sender)) return false;
-    
-    	// if from console they have to enter world name, and if player name world is optional
+	public function execute(CommandSender $sender, string $commandLabel, array $args): bool
+	{
+		if (!$this->testPermission($sender)) {
+			return false;
+		}
+
+		// if from console they have to enter world name, and if player name world is optional
 		$world = null;
-    	if(!$sender instanceof Player){
-    		if(!isset($args[0])){
-    			$sender->sendMessage(TextFormat::RED . "Please input world name");
-    			return false;
-    		}
-    		$world = implode(" ", $args);
-    	} else {
-    		// Check if $world variable null
-			if($world == null){
+		if (!$sender instanceof Player) {
+			if (!isset($args[0])) {
+				$sender->sendMessage(TextFormat::RED . 'Please input world name');
+				return false;
+			}
+			$world = implode(' ', $args);
+		} else {
+			// Check if $world variable null
+			if ($world === null) {
 				// for add custom world optional
-				if(isset($args[0])){
-					$world = implode(" ", $args);
+				if (isset($args[0])) {
+					$world = implode(' ', $args);
 				} else {
 					// if args 0 null this will get player world
 					$world = $sender->getWorld();
@@ -49,10 +54,11 @@ class ClearChunkCommands extends Command implements PluginOwned {
 		}
 		// now clear the world chunk
 		$this->getOwningPlugin()->clearChunk($world, $sender);
-        return true;
+		return true;
 	}
-	
-	public function getOwningPlugin(): AutoClearChunk{
-        return $this->plugin;
-    }
+
+	public function getOwningPlugin(): AutoClearChunk
+	{
+		return $this->plugin;
+	}
 }
